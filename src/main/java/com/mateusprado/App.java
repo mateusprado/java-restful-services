77a60a4bbe276b6ef150a0977f08854d46fe7428
Mateus Prado <mateus@mateusprado.com>
2013-01-09 22:45:02 -0200

    remove comments

--- src/main/java/com/mateusprado/App.java
+++ src/main/java/com/mateusprado/App.java
@@ -1,13 +1,9 @@
 package com.mateusprado;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Java restful!" );
     }
 }
